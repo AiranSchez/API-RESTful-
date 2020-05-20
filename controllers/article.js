eb0024053074535720fc58da763488fa2fb93e52
unknown <airanschez0@gmail.com>
2020-05-20 17:49:01 +0100

    Añadidos métodos del controlador y rutas

--- controllers/article.js
+++ controllers/article.js
@@ -1,24 +1,144 @@
-'use strict'
+"use strict";
+
+var validator = require("validator");
+var Article = require("../models/article");
 
 var controller = {
-    datos: (req, resp) => {
-        return resp.status(200).send({
-            autor: 'Airan',
-            web: 'AiranSchez.com'
-        })
-    },
-    test: (req, resp) => {
-        return resp.status(200).send({
-            message: 'Soy la acción test de mi controlador'
-        })
-    },
+  datos: (req, resp) => {
+    return resp.status(200).send({
+      autor: "Airan",
+      web: "AiranSchez.com",
+    });
+  },
+  test: (req, resp) => {
+    return resp.status(200).send({
+      message: "Soy la acción test de mi controlador",
+    });
+  },
 
-    save: (req, resp) => {
+  save: (req, resp) => {
+    // Recoger parametros por post
+    var params = req.body;
 
-        return resp.status(200).send({
-            message: 'Soy la acción SAVE de mi controlador de artículos'
-        })
+    // Validar datos (validator)
+    try {
+      var validate_title = !validator.isEmpty(params.title);
+      var validate_content = !validator.isEmpty(params.content);
+    } catch (err) {
+      return resp.status(200).send({
+        status: "error",
+        message: "Faltan datos por enviar",
+      });
     }
-} // Fin del controlador
 
-module.exports = controller
+    if (validate_content && validate_title) {
+      // Crear el objeto a guardar
+      var article = new Article();
+
+      // Asignar valores
+      article.title = params.title;
+      article.content = params.content;
+      article.image = null;
+      // Guardar el artículo
+      article.save((error, articleStored) => {
+        if (error || !articleStored) {
+          return resp.status(200).send({
+            status: "error",
+            message: "el articulo no se ha guardado",
+          });
+        } else {
+          return resp.status(200).send({
+            status: "Success",
+            article: articleStored,
+          });
+        }
+      });
+
+      // Devolver una respuesta
+      return resp.status(200).send({
+        status: "Success",
+        article: params,
+      });
+    } else {
+      // Crear el objeto a guardar
+
+      // Asignar valores
+
+      // Guardar el artículo
+
+      // Devolver una respuesta
+      return resp.status(200).send({
+        status: "error",
+        message: "Los datos no son validos",
+      });
+    }
+  },
+
+  getArticles: (req, resp) => {
+    var query = Article.find({});
+    var last = req.params.last;
+
+    if (last || last != undefined) {
+      query.limit(5);
+    }
+    // Find
+    query.sort("-_id").exec((err, articles) => {
+      if (err) {
+        return resp.status(200).send({
+          status: "error",
+          message: "Error al devolver los artículos",
+        });
+      }
+
+      if (!articles) {
+        return resp.status(200).send({
+          status: "error",
+          message: "No hay artículos para mostrar",
+        });
+      }
+
+      return resp.status(200).send({
+        status: "Success",
+        message: "Aqui tienes tus artículos",
+        articles,
+      });
+    });
+  },
+
+  getArticle: (req, resp) => {
+    // Recoger id de la URL
+    var articleId = req.params.id;
+    // Comprobar si existe
+    if (!articleId || articleId == null) {
+      return resp.status(404).send({
+        status: "error",
+        message: "No hay artículo",
+      });
+    }
+    // Buscar artículo
+    Article.findById(articleId, (err, article) => {
+      if (err) {
+        return resp.status(500).send({
+          status: "error",
+          message: "Error al devolver artículo",
+        });
+      }
+
+      if (!articleId) {
+        return resp.status(404).send({
+          status: "error",
+          message: "No existe artículo",
+        });
+      }
+
+      // Devolverlo en json
+      return resp.status(200).send({
+        status: "success",
+        message: "Exito",
+        article,
+      });
+    });
+  },
+}; // Fin del controlador
+
+module.exports = controller;
--- routes/article.js
+++ routes/article.js
@@ -1,15 +1,17 @@
-'use strict'
+"use strict";
 
-var express = require('express')
-var ArticleController = require('../controllers/article')
+var express = require("express");
+var ArticleController = require("../controllers/article");
 
 var router = express.Router();
 
 // Rutas de prueba
-router.post('/datos', ArticleController.datos)
-router.get('/test-de-controlador',ArticleController.test)
+router.post("/datos", ArticleController.datos);
+router.get("/test-de-controlador", ArticleController.test);
 
 // Rutas útiles
-router.post('/save', ArticleController.save)
+router.post("/save", ArticleController.save);
+router.get("/articles/:last?", ArticleController.getArticles);
+router.get("/article/:id", ArticleController.getArticle);
 
-module.exports = router
+module.exports = router;
